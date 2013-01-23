339b19affaad3b9b8f2f26a35696744f2420d7bf
rokkit <fss92@pochta.ru>
2013-01-24 00:52:37 +0400

    Creating app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lightoak::Application.config.secret_token = 'b77a194963fdc4a802bcd61b8cf736ed2c6e1b791202c50e1cd851cfdaa1f66b17d1034f3d911e7f6e0113ae65467e052e6502e7200935ed12951b1be095f60d'
